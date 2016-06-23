158f1afefdee9109fd87b33608746a4648cfb547
sergio <travian-arnoll@hotmail.com>
2016-06-23 20:28:48 +0200

    ya tya tya tya tya tya tya tya tya tya tya tya tya tya tya tya tya tya tya taaaaaaaaaaaaaaaaaaa#

--- app/templates/mensaje.php
+++
@@ -1,6 +0,0 @@
-<?php ob_start() ?>
-<?php if(isset($params['mensaje'])) :?>
-<b><span style="color: red;"><?php echo $params['mensaje'] ?></span></b>
-<?php endif; ?>
-<?php $contenido = ob_get_clean() ?>
-<?php include 'layout.php' ?>
--- app/templates/modificarFoto.php
+++
@@ -1,4 +0,0 @@
-<?php ob_start() ?>
-<img class="perfil" src="<?php echo $params['url'] ?>">
-<?php $contenido = ob_get_clean() ?>
-<?php include 'layout.php' ?>
